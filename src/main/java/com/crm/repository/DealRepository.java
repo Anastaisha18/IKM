2a163cc74f6413a002424d975a3b0cb19796aef7
Anastaisha18 <nastyvavilina@gmail.com>
2025-12-25 12:08:24 +0500

    Add files via upload

---
+++ src/main/java/com/crm/repository/DealRepository.java
@@ -0,0 +1,9 @@
+package com.crm.repository;
+
+import com.crm.entity.Deal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DealRepository extends JpaRepository<Deal, Long> {
+}

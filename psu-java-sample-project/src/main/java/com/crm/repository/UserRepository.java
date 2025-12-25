d0475d2cb48b22e700a2b5965507097c6340bc57
Anastaisha18 <nastyvavilina@gmail.com>
2025-12-25 12:42:45 +0500

    Delete psu-java-sample-project directory

--- psu-java-sample-project/src/main/java/com/crm/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.crm.repository;
-
-import com.crm.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
